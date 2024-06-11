6bbf856a7c5cf8fe677b6bea3646bd44f86a9e94
NicPWNs <nic@nicpjones.com>
2024-06-11 18:52:37 -0400

    fix: content-type

--- hello_world/app.py
+++ hello_world/app.py
@@ -1,39 +1,23 @@
-import json
-
-# import requests
-
+#!/bin/bash/python3
 
 def lambda_handler(event, context):
-    """Sample pure Lambda function
 
-    Parameters
-    ----------
-    event: dict, required
-        API Gateway Lambda Proxy Input Format
-
-        Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
-
-    context: object, required
-        Lambda Context runtime methods and attributes
-
-        Context doc: https://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html
-
-    Returns
-    ------
-    API Gateway Lambda Proxy Output Format: dict
-
-        Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
-    """
-
-    # try:
-    #     ip = requests.get("http://checkip.amazonaws.com/")
-    # except requests.RequestException as e:
-    #     # Send some context about this error to Lambda Logs
-    #     print(e)
-
-    #     raise e
+    body = """
+            <!DOCTYPE html>
+            <html>
+                <head>
+                    <title>Lambda Test Page</title>
+                </head>
+                <body>
+                    <h1>Hello Lambda!</h1>
+                </body>
+            </html>
+            """
 
     return {
         "statusCode": 200,
-        "body": "<!DOCTYPE html><html><head><title>Test Page</title></head><body><h1>Hello World!</h1></body></html>"
+        "body": body,
+        "headers": {
+        'Content-Type': 'text/html',
+        }
     }
